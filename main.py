5ebfef104b46c3ed70c2f5771eb0d7ca7236b13a
evgeniynikolaevich <zplacesound@gmail.com>
2019-11-28 06:46:08 +0300

    need begin from something

---
+++ main.py
@@ -0,0 +1,19 @@
+from random import randrange
+import datetime
+
+
+
+def choose_ten():
+    return randrange(3)
+
+def current_time():
+    return datetime.datetime.now()
+
+
+
+
+
+
+def main():
+    pass    
+
